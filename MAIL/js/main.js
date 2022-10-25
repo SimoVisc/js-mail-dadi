f9ff923e29664ad107bf0953a912cfd040162045
SimoVisc <simonaviscardivs@gmail.com>
2022-10-25 15:29:32 +0200

    aggiunge il comando per inserire la mail

--- MAIL/js/main.js
+++ MAIL/js/main.js
@@ -1,6 +1,6 @@
 "use strict"
 // Chiedi all’utente la sua email
-
+let mail= prompt("Inserisci la tua mail")
 // controlla che sia nella lista di chi può accedere
 
 // stampa un messaggio appropriato sull’esito del controllo
