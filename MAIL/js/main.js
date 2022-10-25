84d6de146bcaea66d298ade5c9ab01136e4c7d15
SimoVisc <simonaviscardivs@gmail.com>
2022-10-25 15:41:52 +0200

    aggiunge il comando per verificare che la mail sia nella lista

--- MAIL/js/main.js
+++ MAIL/js/main.js
@@ -2,5 +2,8 @@
 // Chiedi all’utente la sua email
 let mail= prompt("Inserisci la tua mail")
 // controlla che sia nella lista di chi può accedere
-
+const utenti= ("Luca@gmail.com", "francesca@gmail.com", "simona@gmail.com")
+for(i=0; i<utenti.length; i++){
+    console.log(utenti[i])
+}
 // stampa un messaggio appropriato sull’esito del controllo
