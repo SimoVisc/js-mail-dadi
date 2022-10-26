e830537c17a0ad564fa3fb6d1bc9c5414c99d2dd
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 14:34:34 +0200

    Aggiunge comando per generare il numero per la cpu

--- DADI/js/main.js
+++ DADI/js/main.js
@@ -1,3 +1,7 @@
 "use strict"
 // 1.genera il numero per il giocatore
 const playerNumber=Math.floor(Math.random()*6)+1;
+// 2.genera il numero per la cpu
+const cpuNumber=math.floor(Math.random()*6)+1;
+
+console.log(playerNumber,cpuNumber)
