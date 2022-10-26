54c3ecc1783c0626d99baf7627e3554ee88e5531
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 14:29:18 +0200

    aggiunge correzzione

--- MAIL/js/main.js
+++ MAIL/js/main.js
@@ -1,9 +1,17 @@
 "use strict"
 // Chiedi all’utente la sua email
-let mail= prompt("Inserisci la tua mail")
-// controlla che sia nella lista di chi può accedere
-const utenti= ("Luca@gmail.com", "francesca@gmail.com", "simona@gmail.com")
-for(i=0; i<utenti.length; i++){
-    console.log(utenti[i])
+const mailList= ["francesco@gmail.com","luca@gmail.com","marco@gmail.com",]
+const userMail=promt("inserisci la tua mail")
+let found=false
+for(let i=0; i<mailList.length; i++){
+   if(userMail===mailList[i]){
+    found=true
+   }
 }
+
 // stampa un messaggio appropriato sull’esito del controllo
+if (found){
+    alert("Accesso consentito")
+}else{
+    alert("accesso negato")
+}
