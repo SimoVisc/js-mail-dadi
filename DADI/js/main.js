8035d9ebf3845940b9c3814b9ff90ec54d0e7072
SimoVisc <simonaviscardivs@gmail.com>
2022-10-27 14:22:39 +0200

    aggiunge i comandi per decidere chi vince

--- DADI/js/main.js
+++ DADI/js/main.js
@@ -5,3 +5,13 @@ const playerNumber=Math.floor(Math.random()*6)+1;
 const cpuNumber=math.floor(Math.random()*6)+1;
 
 console.log(playerNumber,cpuNumber)
+// 3. se il numero del giocatore è > del numero della cpu- vince il giocatore
+// altrimenti se il numero del giocatore è < del numero della cpu- vince la cpu
+// Else i numeri sono uguali- pareggio
+if(playerNumber>cpuNumber){
+    console.log("ha vinto il giocatore")
+}else if(playerNumber<cpuNumber){
+    console.log("ha vinto il computer")
+}else (playerNumber=cpuNumber){
+    console.log("pareggio")
+}
